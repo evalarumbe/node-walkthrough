9773200241c860a95ed6b8b0f5be618801eac466
Eva Larumbe <evalarumbe@gmail.com>
2019-06-25 10:13:31 +0200

    Finished up to 2a

---
+++ messaging.js
@@ -0,0 +1,10 @@
+const showHelp = () => {
+    // handle input
+    // no input to see here
+    const helpText = "Welcome to my app";
+    
+    // print output
+    console.log(helpText);
+}
+
+showHelp();
