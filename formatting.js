3c75d19f096bf05fc5b188ef520caf9ef1887ef8
Eva Larumbe <evalarumbe@gmail.com>
2019-06-25 13:11:03 +0200

    Finished ex3!

--- formatting.js
+++ formatting.js
@@ -1,6 +1,9 @@
 const removeWhitespace = input => {
+    // trim space off the ends
     let result = input.trim();
+
+    // collapse duplicate spaces
+    result = result.replace(/\s+/g, ' ');
+
     console.log(result);
 }
-
-removeWhitespace('       sao   tome');
